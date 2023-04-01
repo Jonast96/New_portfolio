7020d026e5a83ba2fdfe1ce523aeb631d6eab750
Jonast96 <Jon.Astveit@hotmail.com>
2023-04-01 02:37:25 +0200

    first draft of section one

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Header from "./components/Header";
+import "./styles/styles.scss";
+import SectionOne from "./views/SectionOne";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <Header />
+      </header>
+      <main>
+        <SectionOne />
+      </main>
+    </div>
+  );
 }
 
 export default App;
