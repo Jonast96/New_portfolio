d47b9bbbfae110823355d3ceeceab2b432d9fdc7
Jonast96 <Jon.Astveit@hotmail.com>
2023-05-25 02:41:47 +0200

    changed form

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,21 +1,31 @@
-
 import dealDash from "../assets/dealDash.png";
 import auctionHouse from "../assets/auctionHouse.png";
+import holidaze from "../assets/holidaze.png";
 const projects = [
-    {
-        image: dealDash,
-        title: "DealDash",
-        shortDescription: "I developed an e-commerce store using React and Sass over a 6-week period, starting with no prior React experience. The project demonstrates my ability to quickly learn and adapt to new technologies.",
-        repoLink: "https://github.com/Jonast96/DealDash",
-        liveSiteLink: "https://joyful-dodol-3ac527.netlify.app/",
-    },
-    {
-        image: auctionHouse,
-        title: "AuctionHouse",
-        shortDescription: "In this semester project, I built an auction site that enables users to register and log in, earn credits by selling items, bid on listings using credits, and edit or delete their listings.",
-        repoLink: "https://github.com/Jonast96/AuctionHouse",
-        liveSiteLink: "https://spectacular-medovik-5141db.netlify.app/",
-    },
+  {
+    image: holidaze,
+    title: "Holidaze",
+    shortDescription:
+      "This project was my final exam in the frontend development course at Noroff. I built a venue booking site using React and react-bootstrap customized with SASS. It uses a REST API that all the students shared.",
+    repoLink: "https://github.com/Jonast96/Holidaze",
+    liveSiteLink: "https://dev-jon-holidaze.netlify.app/",
+  },
+  {
+    image: dealDash,
+    title: "DealDash",
+    shortDescription:
+      "I developed an e-commerce store using React and Sass over a 6-week period, starting with no prior React experience. The project demonstrates my ability to quickly learn and adapt to new technologies.",
+    repoLink: "https://github.com/Jonast96/DealDash",
+    liveSiteLink: "https://joyful-dodol-3ac527.netlify.app/",
+  },
+  {
+    image: auctionHouse,
+    title: "AuctionHouse",
+    shortDescription:
+      "In this semester project, I built an auction site that enables users to register and log in, earn credits by selling items, bid on listings using credits, and edit or delete their listings.",
+    repoLink: "https://github.com/Jonast96/AuctionHouse",
+    liveSiteLink: "https://spectacular-medovik-5141db.netlify.app/",
+  },
 ];
 
 export default projects;
